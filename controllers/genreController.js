66285f38e6bd8d06302e3a1368f10c6fac4e9bb8
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-14 15:40:23 -0600

    feat update genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -124,10 +124,42 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Genre update GET');
+	const genre = await Genre.findById(req.params.id).exec();
+	res.render('genre_form', { title: 'Create Genre', genre: genre });
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Genre update POST');
-});
+exports.genre_update_post = [
+	// Validate and sanitize the name field.
+	body('name', 'Genre name must contain at least 3 characters')
+		.trim()
+		.isLength({ min: 3 })
+		.escape(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a genre object with escaped / trimmed data and old id.
+		const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render the form again with sanitized values/error messages.
+			res.render('genre_form', {
+				title: 'Create Genre',
+				genre: genre,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid and update record.
+			const updatedGenre = await Genre.findByIdAndUpdate(
+				req.params.id,
+				genre,
+				{}
+			);
+			res.redirect(updatedGenre.url);
+		}
+	}),
+];
