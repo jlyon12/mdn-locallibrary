ec4aff9b0f26bf76ea318682d5f1c70bac7ddb04
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-14 15:31:11 -0600

    feat update author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -154,10 +154,72 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Author update GET');
+	const author = await Author.findById(req.params.id).exec();
+
+	if (author === null) {
+		const err = new Error('Author not found');
+		err.status = 404;
+		return next(err);
+	}
+	res.render('author_form', { title: 'Update Author', author: author });
 });
 
 // Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Author update POST');
-});
+exports.author_update_post = [
+	// Validate and sanitize fields.
+	body('first_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('First name must be specified.')
+		.isAlphanumeric()
+		.withMessage('First name has non-alphanumeric characters.'),
+	body('family_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Family name must be specified.')
+		.isAlphanumeric()
+		.withMessage('Family name has non-alphanumeric characters.'),
+	body('date_of_birth', 'Invalid date of birth')
+		.optional({ values: 'falsy' })
+		.isISO8601()
+		.toDate(),
+	body('date_of_death', 'Invalid date of death')
+		.optional({ values: 'falsy' })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create Author object with escaped / trimmed data and old id
+		const author = new Author({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+			_id: req.params.id,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render('author_form', {
+				title: 'Create Author',
+				author: author,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid. Update Record
+			const updatedAuthor = await Author.findByIdAndUpdate(
+				req.params.id,
+				author,
+				{}
+			);
+			res.redirect(updatedAuthor.url);
+		}
+	}),
+];
--- models/author.js
+++ models/author.js
@@ -34,10 +34,18 @@ AuthorSchema.virtual('date_of_birth_formatted').get(function () {
 		: '';
 });
 
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function () {
+	return DateTime.fromJSDate(this.date_of_birth).toISODate();
+});
+
 AuthorSchema.virtual('date_of_death_formatted').get(function () {
 	return this.date_of_death
 		? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
 		: '';
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function () {
+	return DateTime.fromJSDate(this.date_of_death).toISODate();
 });
 
 AuthorSchema.virtual('lifespan').get(function () {
