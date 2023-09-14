f666ae1fb1b323512b3420f70f5b58fe8f7cb6b3
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-13 19:50:49 -0600

    feat create author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Book = require('../models/book');
 const Author = require('../models/author');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -34,14 +35,79 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Author create form on GET.
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Author create GET');
-});
+exports.author_create_get = (req, res, next) => {
+	res.render('author_form', { title: 'Create Author' });
+};
 
 // Handle Author create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-	res.send('NOT IMPLEMENTED: Author create POST');
-});
+// Handle Author create on POST.
+exports.author_create_post = [
+	// Validate and sanitize fields.
+	body('first_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('First name must be specified.')
+		.isAlphanumeric()
+		.withMessage('First name has non-alphanumeric characters.'),
+	body('family_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Family name must be specified.')
+		.isAlphanumeric()
+		.withMessage('Family name has non-alphanumeric characters.'),
+	body('date_of_birth', 'Invalid date of birth')
+		.optional({ values: 'falsy' })
+		.isISO8601()
+		.toDate(),
+	body('date_of_death', 'Invalid date of death')
+		.optional({ values: 'falsy' })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create Author object with escaped and trimmed data
+		const author = new Author({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render('author_form', {
+				title: 'Create Author',
+				author: author,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid.
+			// Check to see if the author has already been created
+			const authorExists = await Author.findOne({
+				first_name: req.body.first_name,
+				family_name: req.body.family_name,
+			})
+				.collation({ locale: 'en', strength: 2 })
+				.exec();
+			if (authorExists) {
+				// Genre exists, redirect to its detail page.
+				res.redirect(authorExists.url);
+			} else {
+				// Save author.
+				await author.save();
+				// Redirect to new author record.
+				res.redirect(author.url);
+			}
+		}
+	}),
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
