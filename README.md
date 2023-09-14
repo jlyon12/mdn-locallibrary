45355bf767004a1330ed56ce2c52c04ff97d6d01
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-09-14 16:11:59 -0600

    pre-deployment cleanup
    update README
    
    update package.json node ver

---
+++ README.md
@@ -0,0 +1,5 @@
+# MDN Local Library Tutorial
+
+## Description
+
+The project tutorial is provided by MDN. The objective is to learn how to use Express to build a basic library website. Check out the tutorial <a href="https://www.theodinproject.com/lessons/nodejs-basic-informational-site">here</a>.
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const debug = require('debug');
 
 const createError = require('http-errors');
 const express = require('express');
@@ -9,14 +10,24 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog'); // Import routes for "catalog" area of site
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
+
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const mongoDB = process.env.MONGODB_URL;
-main().catch((err) => console.log(err));
+main().catch((err) => debug(err));
 async function main() {
 	await mongoose.connect(mongoDB);
 }
@@ -29,6 +40,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
+		},
+	})
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -50,5 +70,4 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error');
 });
-
 module.exports = app;
